0a53d47490b1cc1c4699694ee3e6d9dfafb12475
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-05-24 17:28:31 +0800

    checkPastLimitSimplified

--- function.js
+++ function.js
@@ -16,17 +16,21 @@ var checkPastLimit = function (limiter, item) {
   return item > limiter;
 };
 
+
+
 var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
 console.log(arr4);
-
 
 var arr5 = mapForEach(arr1, function (item) {
   return checkPastLimit(1, item);
 });
 
 console.log(arr5);
+var checkPastLimitSimplified = function (limiter) {
+  return function (limiter, item) {
+    return item > limiter;
+  }.bind(this, limiter);
+};
 
-
-
-
-
+var arr6 = mapForEach(arr1,checkPastLimitSimplified(1))
+console.log(arr6);
